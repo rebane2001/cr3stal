7365b2f41b0517729df4cd6b9dd75f6bc3f92f7d
solonovamax <solonovamax@12oclockpoint.com>
2022-01-10 17:50:49 -0500

    Code cleanup + rename Util class to Cr3stalUtil

--- src/main/java/com/rebane2001/cr3stal/Cr3stalUtil.java
+++ src/main/java/com/rebane2001/cr3stal/Cr3stalUtil.java
@@ -1,10 +1,12 @@
 package com.rebane2001.cr3stal;
 
+
 import net.minecraft.util.math.Quaternion;
 
-public class Util {
+
+public final class Cr3stalUtil {
     // Rotation of individual cubelets
-    public static Quaternion[] cubeletStatus = {
+    public static final Quaternion[] CUBELET_STATUS = {
             new Quaternion(0, 0, 0, 1),
             new Quaternion(0, 0, 0, 1),
             new Quaternion(0, 0, 0, 1),
@@ -32,56 +34,50 @@ public class Util {
             new Quaternion(0, 0, 0, 1),
             new Quaternion(0, 0, 0, 1)
     };
-
+    
     // Get ID of cublet by position
-    public static int[][][] cubletLookup = {
-            // x
-            {
-                    //y
-                    {17, 9, 0},
-                    {20, 16, 3},
-                    {23, 15, 6}
+    public static final int[][][] CUBLET_LOOKUP = {
+            // x axis
+            {       // y axis
+                    { 17, 9, 0 }, { 20, 16, 3 }, { 23, 15, 6 }
             },
-            {
-                    //y
-                    {18, 10, 1},
-                    {21, -1, 4},
-                    {24, 14, 7}
+            {       // y axis
+                    { 18, 10, 1 }, { 21, -1, 4 }, { 24, 14, 7 }
             },
-            {
-                    //y
-                    {19, 11, 2},
-                    {22, 12, 5},
-                    {25, 13, 8}
+            {       // y axis
+                    { 19, 11, 2 }, { 22, 12, 5 }, { 25, 13, 8 }
             }
     };
-
+    
     // Get cublet IDs of a side
-    public static int[][] cubeSides = {
+    public static final int[][] CUBE_SIDES = {
             // front
-            {0, 1, 2, 3, 4, 5, 6, 7, 8},
+            { 0, 1, 2, 3, 4, 5, 6, 7, 8 },
             // back
-            {19, 18, 17, 22, 21, 20, 25, 24, 23},
+            { 19, 18, 17, 22, 21, 20, 25, 24, 23 },
             // top
-            {0, 1, 2, 9, 10, 11, 17, 18, 19},
+            { 0, 1, 2, 9, 10, 11, 17, 18, 19 },
             // bottom
-            {23, 24, 25, 15, 14, 13, 6, 7, 8},
+            { 23, 24, 25, 15, 14, 13, 6, 7, 8 },
             // left
-            {17, 9, 0, 20, 16, 3, 23, 15, 6},
+            { 17, 9, 0, 20, 16, 3, 23, 15, 6 },
             // right
-            {2, 11, 19, 5, 12, 22, 8, 13, 25}
+            { 2, 11, 19, 5, 12, 22, 8, 13, 25 }
     };
-
+    
     // Transformations of cube sides
-    public static int[][] cubeSideTransforms = {
-            {0, 0, 1},
-            {0, 0, -1},
-            {0, 1, 0},
-            {0, -1, 0},
-            {-1, 0, 0},
-            {1, 0, 0}
+    public static final int[][] CUBE_SIDE_TRANSFORMS = {
+            { 0, 0, 1 },
+            { 0, 0, -1 },
+            { 0, 1, 0 },
+            { 0, -1, 0 },
+            { -1, 0, 0 },
+            { 1, 0, 0 }
     };
-
+    
+    private Cr3stalUtil() {
+    }
+    
     public static double easeInOutCubic(double t) {
         return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
     }
--- src/main/java/com/rebane2001/cr3stal/RubicsCubeRenderer.java
+++ src/main/java/com/rebane2001/cr3stal/RubicsCubeRenderer.java
@@ -1,5 +1,6 @@
 package com.rebane2001.cr3stal;
 
+
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -8,11 +9,14 @@ import net.minecraft.util.math.Quaternion;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
+
 public class RubicsCubeRenderer {
-
+    // TODO: 2022-01-10 Make this class entirely stateless & static to avoid having to instantiate it for every end crystal 
+    
     private static final int ANIMATION_LENGTH = 400;
+    
     private static final float CUBELET_SCALE = 0.4f;
-
+    
     // Currently rotating side
     private int rotatingSide = 0;
     // front - 0
@@ -21,51 +25,51 @@ public class RubicsCubeRenderer {
     // bottom - 3
     // left - 4
     // right - 5
-
-    private long lastTime = 0;
-
+    
+    private long lastTime = 0L;
+    
     public void render(ModelPart core, MatrixStack matrices, VertexConsumer vertices, int light, int overlay) {
         long currentTime = System.currentTimeMillis();
-
+        
         if (currentTime - ANIMATION_LENGTH > lastTime) {
             // rotate sides and corners
-            int[] currentSide = Util.cubeSides[rotatingSide];
+            int[] currentSide = Cr3stalUtil.CUBE_SIDES[rotatingSide];
             Quaternion[] cubletsTemp = {
-                    Util.cubeletStatus[currentSide[0]],
-                    Util.cubeletStatus[currentSide[1]],
-                    Util.cubeletStatus[currentSide[2]],
-                    Util.cubeletStatus[currentSide[3]],
-                    Util.cubeletStatus[currentSide[4]],
-                    Util.cubeletStatus[currentSide[5]],
-                    Util.cubeletStatus[currentSide[6]],
-                    Util.cubeletStatus[currentSide[7]],
-                    Util.cubeletStatus[currentSide[8]]
+                    Cr3stalUtil.CUBELET_STATUS[currentSide[0]],
+                    Cr3stalUtil.CUBELET_STATUS[currentSide[1]],
+                    Cr3stalUtil.CUBELET_STATUS[currentSide[2]],
+                    Cr3stalUtil.CUBELET_STATUS[currentSide[3]],
+                    Cr3stalUtil.CUBELET_STATUS[currentSide[4]],
+                    Cr3stalUtil.CUBELET_STATUS[currentSide[5]],
+                    Cr3stalUtil.CUBELET_STATUS[currentSide[6]],
+                    Cr3stalUtil.CUBELET_STATUS[currentSide[7]],
+                    Cr3stalUtil.CUBELET_STATUS[currentSide[8]]
             };
-
+            
             // rotation direction
-            if (true) {
-                Util.cubeletStatus[currentSide[0]] = cubletsTemp[6];
-                Util.cubeletStatus[currentSide[1]] = cubletsTemp[3];
-                Util.cubeletStatus[currentSide[2]] = cubletsTemp[0];
-                Util.cubeletStatus[currentSide[3]] = cubletsTemp[7];
-                Util.cubeletStatus[currentSide[4]] = cubletsTemp[4];
-                Util.cubeletStatus[currentSide[5]] = cubletsTemp[1];
-                Util.cubeletStatus[currentSide[6]] = cubletsTemp[8];
-                Util.cubeletStatus[currentSide[7]] = cubletsTemp[5];
-                Util.cubeletStatus[currentSide[8]] = cubletsTemp[2];
-            } else {
-                Util.cubeletStatus[currentSide[0]] = cubletsTemp[2];
-                Util.cubeletStatus[currentSide[1]] = cubletsTemp[5];
-                Util.cubeletStatus[currentSide[2]] = cubletsTemp[8];
-                Util.cubeletStatus[currentSide[3]] = cubletsTemp[1];
-                Util.cubeletStatus[currentSide[4]] = cubletsTemp[4];
-                Util.cubeletStatus[currentSide[5]] = cubletsTemp[7];
-                Util.cubeletStatus[currentSide[6]] = cubletsTemp[0];
-                Util.cubeletStatus[currentSide[7]] = cubletsTemp[3];
-                Util.cubeletStatus[currentSide[8]] = cubletsTemp[6];
-            }
-
-            int[] trans = Util.cubeSideTransforms[rotatingSide];
+//            if (true) {
+            Cr3stalUtil.CUBELET_STATUS[currentSide[0]] = cubletsTemp[6];
+            Cr3stalUtil.CUBELET_STATUS[currentSide[1]] = cubletsTemp[3];
+            Cr3stalUtil.CUBELET_STATUS[currentSide[2]] = cubletsTemp[0];
+            Cr3stalUtil.CUBELET_STATUS[currentSide[3]] = cubletsTemp[7];
+            Cr3stalUtil.CUBELET_STATUS[currentSide[4]] = cubletsTemp[4];
+            Cr3stalUtil.CUBELET_STATUS[currentSide[5]] = cubletsTemp[1];
+            Cr3stalUtil.CUBELET_STATUS[currentSide[6]] = cubletsTemp[8];
+            Cr3stalUtil.CUBELET_STATUS[currentSide[7]] = cubletsTemp[5];
+            Cr3stalUtil.CUBELET_STATUS[currentSide[8]] = cubletsTemp[2];
+//            } else {
+//                Util.cubeletStatus[currentSide[0]] = cubletsTemp[2];
+//                Util.cubeletStatus[currentSide[1]] = cubletsTemp[5];
+//                Util.cubeletStatus[currentSide[2]] = cubletsTemp[8];
+//                Util.cubeletStatus[currentSide[3]] = cubletsTemp[1];
+//                Util.cubeletStatus[currentSide[4]] = cubletsTemp[4];
+//                Util.cubeletStatus[currentSide[5]] = cubletsTemp[7];
+//                Util.cubeletStatus[currentSide[6]] = cubletsTemp[0];
+//                Util.cubeletStatus[currentSide[7]] = cubletsTemp[3];
+//                Util.cubeletStatus[currentSide[8]] = cubletsTemp[6];
+//            }
+            
+            int[] trans = Cr3stalUtil.CUBE_SIDE_TRANSFORMS[rotatingSide];
             for (int x = -1; x < 2; x++) {
                 for (int y = -1; y < 2; y++) {
                     for (int z = -1; z < 2; z++) {
@@ -77,9 +81,9 @@ public class RubicsCubeRenderer {
             rotatingSide = ThreadLocalRandom.current().nextInt(0, 5 + 1);
             lastTime = currentTime;
         }
-
+        
         matrices.scale(CUBELET_SCALE, CUBELET_SCALE, CUBELET_SCALE);
-
+        
         // Draw non-rotating cubes
         for (int x = -1; x < 2; x++) {
             for (int y = -1; y < 2; y++) {
@@ -89,17 +93,17 @@ public class RubicsCubeRenderer {
                 }
             }
         }
-
+        
         // Draw rotating cubes
-        int[] trans = Util.cubeSideTransforms[rotatingSide];
+        int[] trans = Cr3stalUtil.CUBE_SIDE_TRANSFORMS[rotatingSide];
         matrices.push();
         matrices.translate(trans[0] * CUBELET_SCALE, trans[1] * CUBELET_SCALE, trans[2] * CUBELET_SCALE);
         //GlStateManager.rotate((currentTime - lastTime) * 90 / ANIMATION_LENGTH, trans[0], trans[1], trans[2]);
-        float RotationAngle = (float) Math.toRadians(Util.easeInOutCubic(((float) (currentTime - lastTime)) / ANIMATION_LENGTH) * 90);
-        float xx = (float) (trans[0] * Math.sin(RotationAngle / 2));
-        float yy = (float) (trans[1] * Math.sin(RotationAngle / 2));
-        float zz = (float) (trans[2] * Math.sin(RotationAngle / 2));
-        float ww = (float) Math.cos(RotationAngle / 2);
+        float rotationAngle = (float) Math.toRadians(Cr3stalUtil.easeInOutCubic(((float) (currentTime - lastTime)) / ANIMATION_LENGTH) * 90);
+        float xx = (float) (trans[0] * Math.sin(rotationAngle / 2));
+        float yy = (float) (trans[1] * Math.sin(rotationAngle / 2));
+        float zz = (float) (trans[2] * Math.sin(rotationAngle / 2));
+        float ww = (float) Math.cos(rotationAngle / 2);
         Quaternion qq = new Quaternion(xx, yy, zz, ww);
         matrices.multiply(qq);
         for (int x = -1; x < 2; x++) {
@@ -112,51 +116,54 @@ public class RubicsCubeRenderer {
         }
         matrices.pop();
     }
-
-    private void drawCubeletStatic(ModelPart core, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int x, int y, int z) {
-        int cubletId = Util.cubletLookup[x + 1][y + 1][z + 1];
-
-        if (Arrays.stream(Util.cubeSides[rotatingSide]).anyMatch(r -> r == cubletId))
+    
+    private void drawCubeletStatic(ModelPart core, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int x, int y,
+                                   int z) {
+        int cubletId = Cr3stalUtil.CUBLET_LOOKUP[x + 1][y + 1][z + 1];
+        
+        if (Arrays.stream(Cr3stalUtil.CUBE_SIDES[rotatingSide]).anyMatch(r -> r == cubletId))
             return;
-
+        
         drawCubelet(core, matrices, vertices, light, overlay, x, y, z, cubletId);
     }
-
-    private void drawCubeletRotating(ModelPart core, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int x, int y, int z) {
-        int cubletId = Util.cubletLookup[x + 1][y + 1][z + 1];
-
-        if (Arrays.stream(Util.cubeSides[rotatingSide]).noneMatch(r -> r == cubletId))
+    
+    private void drawCubeletRotating(ModelPart core, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int x, int y,
+                                     int z) {
+        int cubletId = Cr3stalUtil.CUBLET_LOOKUP[x + 1][y + 1][z + 1];
+        
+        if (Arrays.stream(Cr3stalUtil.CUBE_SIDES[rotatingSide]).noneMatch(r -> r == cubletId))
             return;
-
-        int[] trans = Util.cubeSideTransforms[rotatingSide];
+        
+        int[] trans = Cr3stalUtil.CUBE_SIDE_TRANSFORMS[rotatingSide];
         drawCubelet(core, matrices, vertices, light, overlay, x - trans[0], y - trans[1], z - trans[2], cubletId);
     }
-
+    
     private void applyCubeletRotation(int x, int y, int z, int rX, int rY, int rZ) {
-        int cubletId = Util.cubletLookup[x + 1][y + 1][z + 1];
-
-        if (Arrays.stream(Util.cubeSides[rotatingSide]).noneMatch(r -> r == cubletId))
+        int cubletId = Cr3stalUtil.CUBLET_LOOKUP[x + 1][y + 1][z + 1];
+        
+        if (Arrays.stream(Cr3stalUtil.CUBE_SIDES[rotatingSide]).noneMatch(r -> r == cubletId))
             return;
-
-        float RotationAngle = (float) Math.toRadians(90);
-        float xx = (float) (rX * Math.sin(RotationAngle / 2));
-        float yy = (float) (rY * Math.sin(RotationAngle / 2));
-        float zz = (float) (rZ * Math.sin(RotationAngle / 2));
-        float ww = (float) Math.cos(RotationAngle / 2);
+        
+        double rotationAngle = 1.5707963267948966; // Math.toRadians(90)
+        float xx = (float) (rX * Math.sin(rotationAngle / 2));
+        float yy = (float) (rY * Math.sin(rotationAngle / 2));
+        float zz = (float) (rZ * Math.sin(rotationAngle / 2));
+        float ww = (float) Math.cos(rotationAngle / 2);
         Quaternion tempQuat = new Quaternion(xx, yy, zz, ww);
-        tempQuat.hamiltonProduct(Util.cubeletStatus[cubletId]);
-        Util.cubeletStatus[cubletId] = tempQuat;
+        tempQuat.hamiltonProduct(Cr3stalUtil.CUBELET_STATUS[cubletId]);
+        Cr3stalUtil.CUBELET_STATUS[cubletId] = tempQuat;
     }
-
-    private void drawCubelet(ModelPart core, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int x, int y, int z, int cubletId) {
+    
+    private void drawCubelet(ModelPart core, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int x, int y, int z,
+                             int cubletId) {
         matrices.push();
         matrices.translate(x * CUBELET_SCALE, y * CUBELET_SCALE, z * CUBELET_SCALE);
         matrices.push();
-        matrices.multiply(Util.cubeletStatus[cubletId]);
+        matrices.multiply(Cr3stalUtil.CUBELET_STATUS[cubletId]);
         matrices.scale(0.8f, 0.8f, 0.8f);
-
+        
         core.render(matrices, vertices, light, overlay);
-
+        
         matrices.pop();
         matrices.pop();
     }
